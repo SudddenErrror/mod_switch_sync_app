1924f81642a6a53c3fd1415de397aee0e895bddd
SudddenErrror <cheninden@yahoo.com>
2025-06-23 20:07:39 +0300

    // если строка содержит переносы 	// добавить в список ошибок сообщение о том, что файл содержит больше одной строки

--- sync.h
+++ sync.h
@@ -2,11 +2,21 @@
 #define SYNC_H
 
 #include "error.h"
+#include "error.cpp"
 #include <QObject>
+#include <QSet>
 
 int error_count = 0;
 
-void readUserActions (QString actions, QList<int> &actionAfterAction, QSet<Error> &errors);
+void readUserActions (QString actions, QList<int> &actionAfterAction, QSet<Error> &errors)
+{
+    if (actions.contains("\n"))
+    {
+        Error one;
+        one.type = Error::moreThanOneString;
+        errors.insert(one);
+    }
+}
 
 void readUserAccordances (QString accords, QMap<int, QString> &accordModeAction, QSet<Error> &errors);
 
